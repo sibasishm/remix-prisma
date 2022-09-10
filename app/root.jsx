5909a04456b819f6a967f4204fe99ae2c930b912
Sibasish Mohanty <sibasish.mohanty@jupiter.money>
2022-09-10 19:19:06 +0530

    bootstrap a remix app

---
+++ app/root.jsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+export default function App() {
+	const [count, setCount] = React.useState(0);
+	return (
+		<html lang='en'>
+			<head>
+				<meta charset='UTF-8' />
+				<meta http-equiv='X-UA-Compatible' content='IE=edge' />
+				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+				<title>My First Remix App</title>
+			</head>
+			<body>
+				<h1>Welcome to Remix!</h1>
+				<p>Counter is the new hello world.</p>
+				<button
+					onClick={() => {
+						setCount(count + 1);
+					}}
+				>
+					{count}
+				</button>
+			</body>
+		</html>
+	);
+}
