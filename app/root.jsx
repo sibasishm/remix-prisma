b9e623f5e1f86fc1542e92fb32da0f367f7cc395
Sibasish Mohanty <sibasish.mohanty@jupiter.money>
2022-09-10 19:43:15 +0530

    hooray

--- app/root.jsx
+++ app/root.jsx
@@ -6,9 +6,6 @@ export default function App() {
 	return (
 		<html lang='en'>
 			<head>
-				<meta charset='UTF-8' />
-				<meta http-equiv='X-UA-Compatible' content='IE=edge' />
-				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
 				<title>My First Remix App</title>
 			</head>
 			<body>
